c1ff931520f6edfdea8914904bf5bc0193003921
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-25 04:48:48 -0700

    did/syntax.HasPrefixPctEncodedt()

---
+++ syntax/pctencoded.go
@@ -0,0 +1,19 @@
+package syntax
+
+import (
+	"github.com/reiver/go-rfc3986"
+)
+
+// HasPrefixPctEncoded returns true if `str` started with pct-encoded, as defined by https://www.w3.org/TR/did-core/
+//
+// Which is defined as:
+//
+//	pct-encoded = "%" HEXDIG HEXDIG
+//	
+//	HEXDIG      = DIGIT / "A" / "B" / "C" / "D" / "E" / "F"
+//
+// Note that `HEXDIG` does NOT allow lower-case letters "a", "b", "c", "d", "e", "f".
+// Only upper-case letters "A", "B", "C", "D", "E", "F".
+func HasPrefixPctEncoded(str string) bool {
+	return rfc3986.HasPrefixPctEncoded(str)
+}
